5a3d4b404fbccb6ec5a0e64436964fa54c276167
NickolasCamargo <143143466+NickolasCamargo@users.noreply.github.com>
2023-10-26 21:36:03 -0500

    Update script.js
    lo que tengo hasta el momento

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-
 const movableElement = document.getElementById("movableElement");
+const gunGif = document.getElementById("gunGif");
+let gifPlaying = false; // Variable para rastrear si el GIF está en reproducción
+
+gunGif.style.animationPlayState = "paused"; // Pausa la animación por defecto del GIF
+
+gunGif.addEventListener("click", () => {
+    if (gifPlaying) {
+        gunGif.style.animationPlayState = "paused"; // Detener la animación
+    } else {
+        gunGif.style.animationPlayState = "running"; // Iniciar la animación
+    }
+    gifPlaying = !gifPlaying; // Cambiar el estado del GIF
+});
 
 document.addEventListener("mousemove", (event) => {
     const xPosition = event.clientX;
@@ -7,6 +19,31 @@ document.addEventListener("mousemove", (event) => {
     const elementWidth = movableElement.offsetWidth;
     const maxX = windowWidth - elementWidth;
     const newX = Math.min(maxX, Math.max(0, xPosition - elementWidth / 2));
-
     movableElement.style.transform = `translateX(${newX}px)`;
 });
+
+const backgroundSound = document.getElementById("backgroundSound");
+const toggleSoundButton = document.getElementById("toggleSound");
+
+let isSoundEnabled = true; // Inicialmente, el sonido está habilitado
+
+toggleSoundButton.addEventListener("click", () => {
+    if (isSoundEnabled) {
+        backgroundSound.pause(); // Pausar el sonido
+    } else {
+        backgroundSound.play(); // Reproducir el sonido
+    }
+    isSoundEnabled = !isSoundEnabled; // Cambiar el estado del sonido
+});
+// Selecciona el botón de reset
+const resetButton = document.getElementById("reset-button");
+
+// Agrega un evento al botón para volver al principio de la página
+resetButton.addEventListener("click", () => {
+    // Usa el método 'scrollTo' para volver al principio
+    window.scrollTo(0, 0);
+});
+
+
+
+
